8aace776471ecabaa46855ee5e321e03f4a89123
Stanley Lee <sachih@live.com>
2020-01-18 20:32:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
  counter = 0 
  array.each do |values|
    if values == value_to_find
-     counter = values 
+     counter = array.index(values)
      return counter 
    end 
  end 
